d194fbb35d410d0f019bf7263fd219dd132e2e75
karl <karlvorndoenitz@gmail.com>
2017-04-25 10:48:59 +0800

    modified some api

--- database_manager/fields.py
+++ database_manager/fields.py
@@ -5,3 +5,23 @@ __author__ = "karlvorndoenitz@gmail.com"
 
 class Field(object):
     pass
+
+
+class IntegerField(Field):
+    pass
+
+
+class BooleanField(Field):
+    pass
+
+
+class CharField(Field):
+    pass
+
+
+class FloatField(Field):
+    pass
+
+
+class TextField(Field):
+    pass
