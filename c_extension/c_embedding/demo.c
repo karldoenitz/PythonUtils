ff29442247c6322bca634073dddd015db39f52c5
karl <karlvorndoenitz@gmail.com>
2017-10-31 19:43:32 +0800

    增加了C扩展Python的demo

--- c_extension/c_embedding/demo.c
+++ c_extension/c_embedding/demo.c
@@ -1,2 +1,23 @@
 #include <Python.h>
 
+static PyObject *wrap_pass_parameters(PyObject *self, PyObject *args) {
+    char *input_str;
+    if (!PyArg_ParseTuple(args, "s", &input_str)) {
+        return NULL;
+    }
+    PyObject *pyObject = Py_BuildValue("s", result);
+    PyMem_Free(result);
+    return pyObject;
+}
+
+/* registration table  */
+static PyMethodDef wrap_methods[] ={
+        {"pass_parameters", wrap_pass_parameters, METH_VARARGS},       /* method name, C func ptr, always-tuple */
+        {NULL, NULL}                   /* end of table marker */
+};
+
+/* module initializer */
+PyMODINIT_FUNC initencryption(void)                       /* called on first import */
+{                                      /* name matters if loaded dynamically */
+    (void)Py_InitModule("demo", wrap_methods);   /* mod name, table ptr */
+}
