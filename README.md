5eb9cad324e8920d257203c8d350b6d631d8bc70
karl <karlvorndoenitz@gmail.com>
2017-11-15 19:10:54 +0800

    修改了打包模块

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 some utils for python
 ```
 .
+├── c_extension       C/C++扩展Python模块
+├── cdn_utils         cdn相关工具
+├── compress          压缩工具
+├── coroutine         协程
 ├── database_manager  数据库操作
 ├── logger            日志操作
 ├── string_utils      字符串操作
--- compress/operator.py
+++ compress/operator.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+
+operator
+~~~~~~~~
+
+introduction
+use this module to compress folder to *.tar.gz.
+
+Usage
+=====
+>>> from compress import CompressOperator
+>>> compress_operator = CompressOperator("/home/karl/PycharmProjects/PythonUtils", "/home/karl/桌面/result.tar.gz")
+>>> compress_operator.compress(without=["/."])
+>>> compress_operator.package_pyc(without=["/."])
+
+"""
 import os
+import py_compile
 import tarfile
 
 
@@ -13,14 +30,57 @@ class CompressOperator(object):
             for file_name in files:
                 yield os.path.join(root, file_name)
 
-    def compress(self):
+    @classmethod
+    def __is_without(cls, without, file_name):
+        """whether without
+
+        :param without: list type, without files
+        :param file_name: file's name
+        :return: boolean
+
+        """
+        for w in without:
+            if w in file_name:
+                return True
+        return False
+
+    def compress(self, without):
+        """compress to file
+
+        :param without: list type, without files
+        :return: None
+
+        """
         with tarfile.open(self.compress_dir, "w:gz") as tar:
             for file_name in self.__get_file_name():
-                if "/." in file_name:
+                if self.__is_without(without, file_name):
                     continue
                 tar.add(file_name, arcname=file_name.replace(self.folder_dir, ""))
+
+    def package_pyc(self, without):
+        """compile py to pyc and compress file
+
+        :param without: list type, without files
+        :return: None
+
+        """
+        with tarfile.open(self.compress_dir, "w:gz") as tar:
+            file_list = [file_name for file_name in self.__get_file_name()]
+            for file_name in file_list:
+                if self.__is_without(without, file_name):
+                    continue
+                if file_name.endswith(".py"):
+                    pyc_name = file_name + "c"
+                    py_compile.compile(
+                        file=file_name,
+                        cfile=pyc_name
+                    )
+                    tar.add(pyc_name, arcname=pyc_name.replace(self.folder_dir, ""))
+                else:
+                    tar.add(file_name, arcname=file_name.replace(self.folder_dir, ""))
 
 
 if __name__ == '__main__':
     c = CompressOperator("/home/karl/PycharmProjects/PythonUtils", "/home/karl/桌面/result.tar.gz")
-    c.compress()
+    # c.compress(without=["/."])
+    c.package_pyc(without=["/."])
