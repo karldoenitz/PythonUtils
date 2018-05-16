c1d85630d7b8b08f2582795faa800d8e9aaa2a8c
karl <karlvorndoenitz@gmail.com>
2018-05-16 17:54:32 +0800

    增加注释

--- image_utils/__init__.py
+++ image_utils/__init__.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
 
+image_utils
+~~~~~~~~~~~
+
+introduction
+图片处理工具，可以使用这个包内的模块处理图片拓补，以及生成验证码
+
+Usage
+=====
+>>> from image_utils.image_gauss import ImageConverter
+>>> from image_utils.verify_code import VerifyCodeGenerator
+>>> from image_utils import ImageConverter, VerifyCodeGenerator
+
+"""
 from image_gauss import *
 from verify_code import *
